d4692d1f3d45553c702a67e5e624e3ad02961ece
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-04-07 19:24:33 -0300

    ⚡  chore: remove unnecessary icon component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
 'use client';
 
-import React from 'react';
-import { KanbanBoard } from '@/components/organisms/Kanban';
-import { events } from '@/events/modal';
+import { PlusIcon } from 'lucide-react';
+import { events } from '@/events';
 import { ModalsEnum } from '@/types/events';
 import { Clickable } from '@/components/atoms/clickable';
-
-const Icon = () => (
-  <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
-  </svg>
-);
+import { KanbanBoard } from '@/components/organisms/Kanban';
 
 export default function Home() {
   return (
@@ -18,8 +12,8 @@ export default function Home() {
       <div className="container mx-auto max-w-7xl p-4">
         <KanbanBoard.Component>
           <KanbanBoard.Title>Kanban Board</KanbanBoard.Title>
-          <Clickable.Button onClick={() => events.modal.open(ModalsEnum.TASK)}>
-            <Icon />
+          <Clickable.Button onClick={() => events.modal.open({ modal: ModalsEnum.TASK })}>
+            <PlusIcon />
             Adicionar Task
           </Clickable.Button>
         </KanbanBoard.Component>
