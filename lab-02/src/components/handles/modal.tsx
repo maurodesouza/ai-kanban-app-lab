e826a3c5be2e6b49d5ba19c8d2cfe40aa9b269b1
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-04-09 16:34:17 -0300

    🧹 refactor: clean up modal handler component
    - Remove unused React import
    - Remove unused args parameter from handleModalHide
    - Remove redundant comments
    - Simplify return statement comment

--- lab-02/src/components/handles/modal.tsx
+++ lab-02/src/components/handles/modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { events } from '@/events';
 import { Events } from '@/types/events';
 import type { Renderable } from '@/types/helpers';
@@ -11,24 +11,22 @@ function ModalHandler() {
     // TODO: Update modal store with new modal content
   }
 
-  function handleModalHide(args: unknown) {
-    console.log('Modal hide event received:', args);
+  function handleModalHide() {
+    console.log('Modal hide event received');
     // TODO: Update modal store to hide modal
   }
 
   useEffect(() => {
-    // Subscribe to modal events
     events.on(Events.MODAL_SHOW, handleModalShow);
     events.on(Events.MODAL_HIDE, handleModalHide);
     
     return () => {
-      // Cleanup event subscriptions
       events.off(Events.MODAL_SHOW, handleModalShow);
       events.off(Events.MODAL_HIDE, handleModalHide);
     };
   }, []);
 
-  return null; // Handler components are typically invisible
+  return null;
 }
 
 export { ModalHandler };
