cf9f9174973262028cddd55bf30d25f766540391
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-04-10 15:19:01 -0300

    🧼  lint: add prettier and editor config

--- lab-03/eslint.config.mjs
+++ lab-03/eslint.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // Add Prettier config first (to disable conflicting rules)
+  prettierConfig,
+  // Add Prettier plugin
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
