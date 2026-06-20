358f18b2b5eb151d2086116c294d061fdc0fc508
Mauro de Souza <maurodesouza.me@gmail.com>
2026-06-20 18:52:28 -0300

    ✨ feat: add core utils
    Add random.id and debounce helpers used by the kanban store and
    filter component.
    
    Refs #47

---
+++ lab-05/src/utils/debounce/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Creates a debounced function that delays invoking `func` until after `wait` milliseconds
+ * have elapsed since the last time the debounced function was invoked.
+ *
+ * @param func - The function to debounce
+ * @param wait - The number of milliseconds to delay
+ * @returns A new debounced function
+ */
+export function debounce<T extends (...args: unknown[]) => unknown>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeoutId: NodeJS.Timeout | null = null;
+
+  return (...args: Parameters<T>) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, wait);
+  };
+}
